8711198ed1a0de18488b29269e0263c8212bf129
Matan Amichai <datan4@gmail.com>
2022-08-09 14:32:22 +0300

    commit

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,3 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+public class Cal
+{
+    public static void FirstNaiv()
+    {
+        int num1;
+        int num2;
+        int result;
+        Console.WriteLine("This Calculat the sum of 2 number \n Enter the first number ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(" Enter the second number");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        result = num1 + num2;
+        Console.WriteLine($"{num1} + {num2} = {result}");
+    }
+    public static void SecNaiv()
+    {
+        int num1;
+        int num2;
+        int result;
+        bool isSum;
+        Console.WriteLine("This Calculat the sum\\diff of 2 number \n What you like to calculat ? if sum enter 1 else enter 0");
+        isSum = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+        Console.WriteLine("Enter the first number ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the second number");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        if (isSum)
+        {
+            result = num1 + num2;
+
+            Console.WriteLine($"{num1} + {num2} = {result}");
+        }
+        else
+        {
+            result = num1 - num2;
+            Console.WriteLine($"{num1} - {num2} = {result}");
+        }
+
+    }
+
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Cal.FirstNaiv();
+        Cal.SecNaiv();
+    }
+}
